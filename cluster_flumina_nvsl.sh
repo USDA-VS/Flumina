3b4217dac90f09d786ba3da8b4a7200647bf5dbc
tstuber <tod.p.stuber@usda.gov>
2024-05-04 11:02:13 -0500

    file organize and irma TMP change

--- cluster_flumina_nvsl.sh
+++ cluster_flumina_nvsl.sh
@@ -22,5 +22,21 @@ sed -i "s#^READ_DIRECTORY=.*#READ_DIRECTORY=$curdir#" ${new_config}
 sed -i "s#^RENAME_FILE=.*#RENAME_FILE=$new_rename#" ${new_config}
 
 #run Flumina
-sbatch -D ~/git/_github/Flumina ~/git/_github/Flumina/flumina_nvsl.sh ${new_config}
+sbatch -W -D ~/git/_github/Flumina ~/git/_github/Flumina/flumina_nvsl.sh ${new_config}
 
+for i in ~/git/_github/Flumina/IRMA_results/*; do name=$(basename $i); mkdir -p $curdir/sample_gathering/$name; echo $name >> sample_list; done
+while read i; do cp -r ~/git/_github/Flumina/BAM_files/$i $curdir/sample_gathering/$i/BAM_files; done < sample_list
+while read i; do mkdir -p $curdir/sample_gathering/$i/IRMA-consensus-contigs; cp -v ~/git/_github/Flumina/IRMA-consensus-contigs/${i}.fasta $curdir/sample_gathering/$i/IRMA-consensus-contigs; done < sample_list
+while read i; do cp -r ~/git/_github/Flumina/IRMA_results/$i $curdir/sample_gathering/$i/IRMA_results; done < sample_list
+while read i; do cp -r ~/git/_github/Flumina/logs/$i $curdir/sample_gathering/$i/logs; done < sample_list
+while read i; do cp -r ~/git/_github/Flumina/processed-reads/$i $curdir/sample_gathering/$i/processed-reads; done < sample_list
+while read i; do cp -r ~/git/_github/Flumina/vcf_files/$i $curdir/sample_gathering/$i/vcf_files; done < sample_list
+while read i; do mkdir -p $curdir/sample_gathering/$i/variant_analysis; cp -v ~/git/_github/Flumina/variant_analysis/aa_db/${i}.csv $curdir/sample_gathering/$i/variant_analysis; done < sample_list
+
+mkdir -p $curdir/sample_gathering/run_${dd}
+cp ~/git/_github/Flumina/slurm* $curdir/sample_gathering/run_${dd}
+cp $curdir/config* $curdir/sample_gathering/run_${dd}
+cp $curdir/rename* $curdir/sample_gathering/run_${dd}
+cp $curdir/slurm* $curdir/sample_gathering/run_${dd}
+cp ~/git/_github/Flumina/variant_analysis/*.txt $curdir/sample_gathering/run_${dd}
+cp ~/git/_github/Flumina/variant_analysis/*.csv $curdir/sample_gathering/run_${dd}
--- irma_config.sh
+++ irma_config.sh
@@ -1,3 +1,3 @@
-TMP=/Flumina_test/Flumina/irma_tmp
+TMP=~/Flumina_test/Flumina/irma_tmp
 SINGLE_LOCAL_PROC=4
 DOUBLE_LOCAL_PROC=2
