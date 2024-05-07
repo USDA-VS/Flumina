3515d9703cced28018daa300a6cb512e6ed259f6
Cameron.Norris <Cameron.Norris@usda.gov>
2024-05-07 08:07:17 -0500

    output writes to flumina_out folder in cwd

--- cluster_flumina_nvsl.sh
+++ cluster_flumina_nvsl.sh
@@ -5,8 +5,9 @@
 
 dd=`date +"%Y-%m-%d_%H-%M-%S"`
 curdir=`pwd`
-module load anaconda3
-source activate /project/shared/anaconda_env/Flumina
+flu_out=$curdir/flumina_out
+# module load anaconda3
+# source activate /project/shared/anaconda_env/Flumina
 
 # copy and edit config file
 new_config=${curdir}/config_${dd}.cfg
@@ -15,28 +16,31 @@ cp ~/git/_github/Flumina/config.cfg ${new_config}
 
 # generate new rename file, splitting on _ and grabbing the first field. Doesn't currently rename, but file is required
 printf "File,Sample\n" > ${new_rename}
-ls -1 *fastq.gz| awk -F_ '{print $1","$1}' >> ${new_rename}
+for i in $(ls *fastq.gz | awk -F_ '{print $1}'); do grep -q $i ${new_rename} || echo $i,$i >> ${new_rename}; done
+
 
 # replace rename file and read directory in the config file
 sed -i "s#^READ_DIRECTORY=.*#READ_DIRECTORY=$curdir#" ${new_config}
 sed -i "s#^RENAME_FILE=.*#RENAME_FILE=$new_rename#" ${new_config}
+sed -i "s#^OUTPUT_DIRECTORY=.*#OUTPUT_DIRECTORY=$flu_out#" ${new_config}
 
 #run Flumina
 sbatch -W -D ~/git/_github/Flumina ~/git/_github/Flumina/flumina_nvsl.sh ${new_config}
 
-for i in ~/git/_github/Flumina/IRMA_results/*; do name=$(basename $i); mkdir -p $curdir/sample_gathering/$name; echo $name >> sample_list; done
-while read i; do cp -r ~/git/_github/Flumina/BAM_files/$i $curdir/sample_gathering/$i/BAM_files; done < sample_list
-while read i; do mkdir -p $curdir/sample_gathering/$i/IRMA-consensus-contigs; cp -v ~/git/_github/Flumina/IRMA-consensus-contigs/${i}.fasta $curdir/sample_gathering/$i/IRMA-consensus-contigs; done < sample_list
-while read i; do cp -r ~/git/_github/Flumina/IRMA_results/$i $curdir/sample_gathering/$i/IRMA_results; done < sample_list
-while read i; do cp -r ~/git/_github/Flumina/logs/$i $curdir/sample_gathering/$i/logs; done < sample_list
-while read i; do cp -r ~/git/_github/Flumina/processed-reads/$i $curdir/sample_gathering/$i/processed-reads; done < sample_list
-while read i; do cp -r ~/git/_github/Flumina/vcf_files/$i $curdir/sample_gathering/$i/vcf_files; done < sample_list
-while read i; do mkdir -p $curdir/sample_gathering/$i/variant_analysis; cp -v ~/git/_github/Flumina/variant_analysis/aa_db/${i}.csv $curdir/sample_gathering/$i/variant_analysis; done < sample_list
+cd ./flumina_out
+for i in ./IRMA_results/*; do name=$(basename $i); mkdir -p $flu_out/sample_gathering/$name; echo $name >> sample_list; done
+while read i; do cp -r ./BAM_files/$i $flu_out/sample_gathering/$i/BAM_files; done < sample_list
+while read i; do mkdir -p $flu_out/sample_gathering/$i/IRMA-consensus-contigs; cp -v ./IRMA-consensus-contigs/${i}.fasta $flu_out/sample_gathering/$i/IRMA-consensus-contigs; done < sample_list
+while read i; do cp -r ./IRMA_results/$i $flu_out/sample_gathering/$i/IRMA_results; done < sample_list
+while read i; do cp -r ./logs/$i $flu_out/sample_gathering/$i/logs; done < sample_list
+while read i; do cp -r ./processed-reads/$i $flu_out/sample_gathering/$i/processed-reads; done < sample_list
+while read i; do cp -r ./vcf_files/$i $flu_out/sample_gathering/$i/vcf_files; done < sample_list
+while read i; do mkdir -p $flu_out/sample_gathering/$i/variant_analysis; cp -v ./variant_analysis/aa_db/${i}.csv $flu_out/sample_gathering/$i/variant_analysis; done < sample_list
 
-mkdir -p $curdir/sample_gathering/run_${dd}
-cp ~/git/_github/Flumina/slurm* $curdir/sample_gathering/run_${dd}
-cp $curdir/config* $curdir/sample_gathering/run_${dd}
-cp $curdir/rename* $curdir/sample_gathering/run_${dd}
-cp $curdir/slurm* $curdir/sample_gathering/run_${dd}
-cp ~/git/_github/Flumina/variant_analysis/*.txt $curdir/sample_gathering/run_${dd}
-cp ~/git/_github/Flumina/variant_analysis/*.csv $curdir/sample_gathering/run_${dd}
+mkdir -p $flu_out/sample_gathering/run_${dd}
+mv ~/git/_github/Flumina/slurm* $flu_out/sample_gathering/run_${dd}
+mv $curdir/config* $flu_out/sample_gathering/run_${dd}
+mv $curdir/rename* $flu_out/sample_gathering/run_${dd}
+mv $curdir/slurm* $flu_out/sample_gathering/run_${dd}
+cp ./variant_analysis/*.txt $flu_out/sample_gathering/run_${dd}
+cp ./variant_analysis/*.csv $flu_out/sample_gathering/run_${dd}
