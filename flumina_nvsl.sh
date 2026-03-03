eaaa1996b0761f5a3dcfb32b3758c7394fe8f967
Reed Woyda <49286465+BioRRW@users.noreply.github.com>
2026-03-03 14:34:29 -0700

    Update flumina_nvsl.sh

--- flumina_nvsl.sh
+++ flumina_nvsl.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH --job-name=Flumina-nvsl
-#SBATCH -N 1
-#SBATCH -p prod-compute-mem
+#SBATCH --account="aap mr scicomp hpc cnah users" 
+#SBATCH --partition=scicomp-compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=100G
 #SBATCH -t 168:00:00
-
+#SBATCH --export=none
 
 date
 
-module purge
-module load slurm
-module load anaconda3
+# REMOVED: module purge, module load, and source activate
+# The Apptainer container natively supplies the 'dvl_irma' conda environment.
 
-source activate /project/shared/anaconda_env/Flumina
-
-bash ~/git/_github/Flumina/Flumina ${1}
+# FIXED: Changed ~/git to the absolute container path /git
+bash /git/_github/Flumina/Flumina ${1}
 
 date
 
 # End of file
-
