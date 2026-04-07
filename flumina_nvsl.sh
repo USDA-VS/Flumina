5d97d1795614725819824ab18b01feb9be0241c9
stuber <tod.p.stuber@usda.gov>
2026-04-07 10:56:18 -0600

    Update flumina_nvsl.sh
    conda activate and PATH update

--- flumina_nvsl.sh
+++ flumina_nvsl.sh
@@ -9,13 +9,18 @@ date
 
 module purge
 module load slurm
-module load anaconda3
+#module load anaconda3
+source /project/shared/miniconda3/etc/profile.d/conda.sh
 
-source activate /project/shared/anaconda_env/Flumina
-
-bash ~/git/_github/Flumina/Flumina ${1}
+#source activate /project/shared/anaconda_env/Flumina
+conda activate /project/shared/anaconda_env/Flumina
+export PATH="/project/shared/anaconda_env/Flumina/bin:$PATH"
 
 date
+# Verify conda R is being used
+echo "Rscript path: $(which Rscript)"
+echo "R path: $(which R)"
+
+ bash ~/git/_github/Flumina/Flumina ${1}
 
 # End of file
-
